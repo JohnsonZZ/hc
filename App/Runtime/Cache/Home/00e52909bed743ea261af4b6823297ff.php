1f299944ee2f1fbdd4dac05484432665b777eb33
JohnsonZZ <465629989@qq.com>
2016-04-24 23:20:03 +0800

    update index

--- App/Runtime/Cache/Home/00e52909bed743ea261af4b6823297ff.php
+++ App/Runtime/Cache/Home/00e52909bed743ea261af4b6823297ff.php
@@ -1,16 +1,47 @@
-<!DOCTYPE html>
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
     <head>
-        <include file="Public/header" title="华创信息科技公司"/>
+        <meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title>华创信息科技公司</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width">
+
+<link rel="stylesheet" href="/hc/Bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="/hc/Public/css/leaflet.css" />
+<link rel="icon" href="/hc/Public/images/title-logo.png">
+<!--[if lte IE 8]>
+	 <link rel="stylesheet" href="css/leaflet.ie.css" />
+<![endif]-->
+<link rel="stylesheet" href="/hc/Public/css/main.css">
+<script src="/hc/Public/js/modernizr-2.6.2-respond-1.1.0.min.js"></script>
     </head>
     <body>
         <!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
-        <include file="Public/nav" />
+        <!-- Navigation & Logo-->
+<div class="mainmenu-wrapper">
+	<div class="container">
+		<nav id="mainmenu" class="mainmenu">
+			<ul>
+				<li class="logo-wrapper"><a href="<?php echo U('Index/index');?>"><img src="/hc/Public/images/hc-logo.png" alt="Multipurpose Twitter Bootstrap Template"></a></li>
+				<li id="index" class="active">
+					<a href="<?php echo U('Index/index');?>">首页</a>
+				</li>
+				<li id="busi">
+					<a href="<?php echo U('Busi/index');?>">主营业务</a>
+				</li>
+				<li id="news">
+					<a href="<?php echo U('News/index');?>">公司动态</a>
+				</li>
+			</ul>
+		</nav>
+	</div>
+</div>
         <!-- Homepage Slider -->
         <div class="homepage-slider">
         	<div id="sequence">
@@ -22,7 +53,7 @@
 						<!-- Slide Text -->
 						<h3 class="subtitle">如果你需要帮助而我正好有能力帮你，那我会尽全力为你提供最优质的服务！</h3>
 						<!-- Slide Image -->
-						<img class="slide-img" src="__PUBLIC__/images/homepage-slider/slide1.png" alt="Slide 1" />
+						<img class="slide-img" src="/hc/Public/images/homepage-slider/slide1.png" alt="Slide 1" />
 					</li>
 					<!-- End Slide 1 -->
 					<!-- Slide 2 -->
@@ -32,7 +63,7 @@
 						<!-- Slide Text -->
 						<h3 class="subtitle">因您所求，做您所需！</h3>
 						<!-- Slide Image -->
-						<img class="slide-img" src="__PUBLIC__/images/homepage-slider/slide2.png" alt="Slide 2" />
+						<img class="slide-img" src="/hc/Public/images/homepage-slider/slide2.png" alt="Slide 2" />
 					</li>
 					<!-- End Slide 2 -->
 					<!-- Slide 3 -->
@@ -46,7 +77,7 @@
 						地址：肇庆市端州区肇庆学院中巴软件楼
 						</h3>
 						<!-- Slide Image -->
-						<img class="slide-img" src="__PUBLIC__/images/homepage-slider/slide3.png" alt="Slide 3" />
+						<img class="slide-img" src="/hc/Public/images/homepage-slider/slide3.png" alt="Slide 3" />
 					</li>
 					<!-- End Slide 3 -->
 				</ul>
@@ -92,7 +123,7 @@
 	        	<div class="row">
 	        		<div class="col-md-4 col-sm-6">
 	        			<div class="service-wrapper">
-		        			<img src="__PUBLIC__/images/service-icon/ruler.png" alt="Service 2">
+		        			<img src="/hc/Public/images/service-icon/ruler.png" alt="Service 2">
 		        			<h3>专业设计</h3>
 		        			<p>掌握时下最热门的技术，模板，独立开发后台管理系统。优化搜索，帮助客户设计个性化软件。优质售后，客户再无后顾之忧。</p>
 		        			<a href="#" class="btn">查看更多</a>
@@ -100,7 +131,7 @@
 	        		</div>
 	        		<div class="col-md-4 col-sm-6">
 	        			<div class="service-wrapper">
-		        			<img src="__PUBLIC__/images/service-icon/box.png" alt="Service 3">
+		        			<img src="/hc/Public/images/service-icon/box.png" alt="Service 3">
 		        			<h3>技术团队</h3>
 		        			<p>我们是一个成熟的团队，团队成员都有3年以上的开发经验，主要从事以下几项工作：前端重构，页面重构，网站建设，门户建设等等</p>
 		        			<a href="#" class="btn">查看更多</a>
@@ -108,7 +139,7 @@
 	        		</div>
 					<div class="col-md-4 col-sm-6">
 	        			<div class="service-wrapper">
-		        			<img src="__PUBLIC__/images/service-icon/diamond.png" alt="Service 1">
+		        			<img src="/hc/Public/images/service-icon/diamond.png" alt="Service 1">
 		        			<h3>优质服务</h3>
 		        			<p>不断创新，为客户提供全面更优质的开发及后续服务，利用自己独到的优势，协助客户在日益激烈的信息时代竞争中立于不败之地</p>
 		        			<a href="#" class="btn">查看更多</a>
@@ -118,9 +149,62 @@
 	        </div>
 	    </div>
 	    <!-- End Services -->
-	    <include file="Public/footer" />
-        <script src="__PUBLIC__/js/jquery.sequence-min.js"></script>
-		<script src="__PUBLIC__/js/template.js"></script>
+	    <!-- Footer -->
+<div class="footer">
+	<div class="container">
+		<div class="row">
+		    <div class="col-footer col-sm-2 col-xs-4">
+		    	<h3>关于我们</h3>
+		    	<ul class="no-list-style footer-navigate-section">
+		    		<li><a>关于我们</a></li>
+		    		<li><a>服务范围</a></li>
+		    		<li><a>网站开发</a></li>
+		    		<li><a>联系我们</a></li>
+				</ul>
+			</div>
+		    <div class="col-footer col-sm-2 col-xs-4">
+				<h3>服务范围</h3>
+				<ul class="no-list-style footer-navigate-section">
+					<li>软件外包</li>
+		    		<li>软件开发</li>
+		    		<li>微信平台</li>
+		    		<li>后台系统</li>
+		    	</ul>
+		    </div>
+			<div class="col-footer col-sm-2 col-xs-4">
+				<h3>工作招聘</h3>
+				<ul class="no-list-style footer-navigate-section">
+					<li>美工</li>
+					<li>UI设计</li>
+					<li>前端开发</li>
+		    		<li>后端开发</li>
+		    	</ul>
+		    </div>
+		   	<div class="col-footer col-sm-3 col-xs-6">
+				<h3>联系我们</h3>
+					<ul class="no-list-style footer-navigate-section">
+		   			<li><b>电话:</b> 13828384812</li>
+		   			<li><b>微信:</b> </li>
+		    		<li><b>邮箱:</b> HCIT@gmail.com</li>
+		   			<li><b>地址:</b> 肇庆市端州区肇庆学院中巴软件楼</li>
+		   		</ul>
+		   	</div>
+			<div class="col-footer col-sm-2 col-xs-6">
+				<h3>扫一扫关注我们</h3>
+				<img src="/hc/Public/images/QR-Code.png" alt="关注我们" width="150px"/>
+			</div>
+		</div>
+		<div class="row">
+		    <div class="col-md-12">
+		    	<div class="footer-copyright">&copy; 2016 华创信息科技有限公司. All rights reserved.</div>
+		    </div>
+		</div>
+	</div>
+</div>
+	<script src="/hc/Public/js/jquery.js"></script>
+    <script src="/hc/Bootstrap/js/bootstrap.min.js"></script>
+        <script src="/hc/Public/js/jquery.sequence-min.js"></script>
+		<script src="/hc/Public/js/template.js"></script>
 
     </body>
 </html>
