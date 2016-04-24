9ac254b0fb5b506080710e71e83e7d9df75e20e1
JohnsonZZ <465629989@qq.com>
2016-04-24 23:01:18 +0800

    v1.0.0

---
+++ App/Home/Controller/NewsController.class.php
@@ -0,0 +1,25 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class NewsController extends Controller {
+    public function index(){
+		$List = M('List');
+		$count = $List->count(); // 查询满足要求的总记录数
+		$Page = new \Think\Page($count,10); // 实例化分页类 传入总记录数和每页显示的记录数(10)
+		$list = $List->order('id desc')->limit($Page->firstRow . ',' . $Page->listRows)->select();
+		$Page->setConfig('prev', '<');
+		$Page->setConfig('next', '>');
+		$Page->setConfig('header','');
+		$show = $Page->show(); // 分页显示输出
+		$this->assign('page', $show); // 赋值分页输出
+		$this->assign('list',$list);
+		$this->display();
+	}
+	public function news(){
+		$data['id'] = I('get.theme');
+		$List = M('List');
+		$list = $List->where($data)->find();
+		$this->assign($list);
+		$this->display();
+	}
+}
